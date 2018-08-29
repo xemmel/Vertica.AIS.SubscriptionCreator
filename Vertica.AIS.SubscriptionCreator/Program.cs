d62240be8a225e5aab8b5733e7e2e086275e55e2
xemmel <lacour@gmail.com>
2018-08-29 13:14:20 +0200

    Version 1.0

--- Vertica.AIS.SubscriptionCreator/Program.cs
+++ Vertica.AIS.SubscriptionCreator/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +11,34 @@ namespace Vertica.AIS.SubscriptionCreator
 {
     class Program
     {
+        static string ReadKey(string key)
+        {
+            return ConfigurationManager.AppSettings[key];
+        }
         static void Main(string[] args)
         {
+            try
+            {
+                string connectionString = ReadKey("connectionString");
+                string topicPath = ReadKey("topicPath");
+
+                string subscriptionName = args[0];
+                string filter = args[1];
+                var namespaceManager = NamespaceManager.CreateFromConnectionString(connectionString);
+
+                SubscriptionDescription subscriptionDescription =
+                    new SubscriptionDescription(topicPath, subscriptionName)
+                    {
+                      
+                    };
+                namespaceManager.CreateSubscription(subscriptionDescription, new SqlFilter(filter));
+                Console.WriteLine($"Subscription: {subscriptionName}({filter}) created in topic: {topicPath}");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
